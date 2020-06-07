40004ecfb085812e27bb7a034b627f9d8df0c5b1
Dorkside <jdmartin@gmx.fr>
2020-06-07 11:42:08 +0200

    feat: init

---
+++ src/views/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Calendar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Calendar from "@/components/Calendar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Calendar
+  }
+};
+</script>
